35e8dfbae9bd42095dc5a378e210d92c0ff61d2c
Arnel Francia <nelffff@gmail.com>
2024-09-24 21:00:54 +0800

    updated

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const users = [
+  { id: 1, username: "Arnel", displayName: "Rnel" },
+  { id: 2, username: "Dave", displayName: "Dave" },
+  { id: 3, username: "Raul", displayName: "Raul" },
+];
+
 app.listen(PORT, () => {
   console.log(`Running on the Port ${PORT}`);
 });
@@ -12,9 +18,22 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
-  response.send([
-    { id: 1, username: "Arnel", displayName: "Rnel" },
-    { id: 2, username: "Dave", displayName: "Dave" },
-    { id: 3, username: "Raul", displayName: "Raul" },
-  ]);
+  response.send(users);
+});
+
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    return response.status(400).send("Bad Request. Invalid ID");
+  }
+
+  const findUser = users.find((user) => user.id === parsedId);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
+});
+
+app.get("/api/products", (request, response) => {
+  response.send([{ id: 123, name: " Nuggets", price: 12.99 }]);
 });
