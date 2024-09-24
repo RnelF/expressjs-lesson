ff155e14028be3a8669436888f9513d70276c106
Arnel Francia <nelffff@gmail.com>
2024-09-24 20:42:31 +0800

    updated

--- src/index.mjs
+++ src/index.mjs
@@ -6,3 +6,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Running on the Port ${PORT}`);
 });
+
+app.get("/", (request, response) => {
+  response.status(201).send({ hello: "Hello" });
+});
+
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, username: "Arnel", displayName: "Rnel" },
+    { id: 2, username: "Dave", displayName: "Dave" },
+    { id: 3, username: "Raul", displayName: "Raul" },
+  ]);
+});
