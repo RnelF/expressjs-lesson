cceb402b1856497471e05f9457a85f7d074a1fbf
Arnel Francia <nelffff@gmail.com>
2024-09-25 20:04:40 +0800

    updated

--- src/index.mjs
+++ src/index.mjs
@@ -7,6 +7,11 @@ const users = [
   { id: 1, username: "Arnel", displayName: "Rnel" },
   { id: 2, username: "Dave", displayName: "Dave" },
   { id: 3, username: "Raul", displayName: "Raul" },
+  { id: 4, username: "Niko", displayName: "Niks" },
+  { id: 5, username: "Jomar", displayName: "Jom" },
+  { id: 6, username: "Carl", displayName: "CJ" },
+  { id: 7, username: "Biggy", displayName: "Big" },
+  { id: 8, username: "James", displayName: "Jim" },
 ];
 
 app.listen(PORT, () => {
@@ -18,7 +23,15 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
-  response.send(users);
+  console.log(request.query);
+  const {
+    query: { filter, value },
+  } = request;
+
+  if (!filter && !value) return response.send(users);
+
+  if (filter && value)
+    return response.send(users.filter((user) => user[filter].includes(value)));
 });
 
 app.get("/api/users/:id", (request, response) => {
